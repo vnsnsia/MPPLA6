e8dd315b6ab855d6e4933c7e5fa9f8a4e5d904ce
faishal15 <faishal15@gmail.com>
2017-04-09 17:19:51 +0700

    Update tabel transaksi dan perbaikan tabel lainnya

--- tcari.sql
+++ tcari.sql
@@ -1,67 +1,115 @@
-/*
-SQLyog Ultimate v8.6 Beta2
-MySQL - 5.6.26 : Database - tcari
-*********************************************************************
-*/
-
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-/*Table structure for table `barang` */
-
-DROP TABLE IF EXISTS `barang`;
-
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 09, 2017 at 12:14 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 7.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tcari`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
 CREATE TABLE `barang` (
-  `id_barang` varchar(10) NOT NULL,
-  `nama_barang` varchar(15) DEFAULT NULL,
-  `tanggal` date DEFAULT NULL,
-  `tempat` varchar(20) DEFAULT NULL,
-  `id_user` varchar(15) DEFAULT NULL,
-  `kategori` varchar(20) DEFAULT NULL,
-  `status` varchar(20) DEFAULT NULL,
-  `keterangan` text,
-  `foto` varchar(40) DEFAULT NULL,
-  PRIMARY KEY (`id_barang`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-/*Data for the table `barang` */
-
-/*Table structure for table `message` */
-
-DROP TABLE IF EXISTS `message`;
-
+  `ID_Barang` varchar(10) NOT NULL,
+  `ID_User` varchar(15) NOT NULL,
+  `Nama_Barang` varchar(15) NOT NULL,
+  `Tanggal` datetime NOT NULL,
+  `Tempat` varchar(20) NOT NULL,
+  `Kategori` varchar(20) NOT NULL,
+  `Keterangan` text NOT NULL,
+  `Foto` varchar(40) NOT NULL,
+  `Security_Ques` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
 CREATE TABLE `message` (
-  `id_message` varchar(10) NOT NULL,
-  `judul_message` varchar(25) DEFAULT NULL,
-  `isi_message` text,
-  `id_sender` varchar(15) DEFAULT NULL,
-  `id_receiver` varchar(15) DEFAULT NULL,
-  PRIMARY KEY (`id_message`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-/*Data for the table `message` */
-
-/*Table structure for table `user` */
-
-DROP TABLE IF EXISTS `user`;
-
+  `ID_Message` varchar(10) NOT NULL,
+  `Judul_Message` varchar(25) NOT NULL,
+  `Isi_Message` text NOT NULL,
+  `ID_Sender` varchar(15) NOT NULL,
+  `ID_Receiver` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `ID_Transaksi` varchar(10) NOT NULL,
+  `ID_Barang` varchar(20) NOT NULL,
+  `ID_Pemilik` varchar(20) NOT NULL,
+  `ID_Penemu` varchar(20) NOT NULL,
+  `Tanggal_Selesai` datetime NOT NULL,
+  `Status` varchar(30) NOT NULL,
+  `Progress` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
 CREATE TABLE `user` (
-  `id_user` varchar(15) NOT NULL,
-  `nama_user` varchar(40) DEFAULT NULL,
-  `no_telepon` varchar(15) DEFAULT NULL,
-  `foto` varchar(40) DEFAULT NULL,
-  `password` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-/*Data for the table `user` */
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+  `ID_User` varchar(15) NOT NULL,
+  `Nama_User` varchar(40) NOT NULL,
+  `No_Telepon` varchar(15) NOT NULL,
+  `Foto` varchar(40) NOT NULL,
+  `Password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`ID_Barang`);
+
+--
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`ID_Message`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`ID_Transaksi`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`ID_User`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
